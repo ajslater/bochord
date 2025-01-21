8e7de4f5c6ed3ade9668104361eb55f4c91d31fd
AJ Slater <aj@slater.net>
2025-01-21 01:21:20 -0800

    refactor and combine some printing and file changed logic. deflate epubs

--- bochord/bochord.py
+++ bochord/bochord.py
@@ -1,25 +1,34 @@
 """Backup books from macOS Books to usable ePubs."""
 
 import subprocess
+from pathlib import Path
 
 from termcolor import cprint
 
-from bochord.epub_dir import backup_epub_dir
+from bochord.epub_dir import backup_epub_dir, get_dest_file_mtime
 
 __version__ = "1.1.0"
 
 
-def backup_file(filename, args):
+def backup_file(filename: Path, args):
     """Backup documents that aren't epub directories."""
-    verbose = "-v" if args.verbose else "-q"
-    subprocess.call(["rsync", "-aP", verbose, filename, args.dest])  # noqa: S603,S607
+    src_path = args.source / filename.name
+    src_file_mtime = src_path.stat().st_mtime
+    dest_file_mtime = get_dest_file_mtime(filename, args)
+    if args.force or src_file_mtime > dest_file_mtime:
+        cprint(f"\nArchiving: {filename}", "cyan")
+        verbose = "-v" if args.verbose else "-q"
+        subprocess_args = ("rsync", "-aP", verbose, str(src_path), args.dest)
+        subprocess.call(subprocess_args)  # noqa: S603
+        return True
+    return False
 
 
 def prune(args):
     """Prune docs from destination that aren't in the source."""
     dest_set = set(args.dest.iterdir())
     src_set = set(args.source.iterdir())
-    extra_set = dest_set - src_set
+    extra_set = sorted(dest_set - src_set)
     if args.verbose:
         cprint(f"Removing: {extra_set}", "yellow")
     for filename in extra_set:
@@ -29,11 +38,20 @@ def prune(args):
 
 def run(args):
     """Backup everything."""
-    for filename in args.source.iterdir():
+    for filename in sorted(args.source.iterdir()):
         if filename.suffix == ".epub" and filename.is_dir():
-            backup_epub_dir(filename, args)
+            result = backup_epub_dir(filename, args)
         else:
-            backup_file(filename, args)
+            result = backup_file(filename, args)
+
+        if not result:
+            if args.verbose:
+                cprint(f"\nNot updated: {filename}", "green")
+            else:
+                cprint(".", "green", end="")
+
+    if not args.verbose:
+        cprint("")
 
     if args.prune:
         prune(args)
--- bochord/epub_dir.py
+++ bochord/epub_dir.py
@@ -3,40 +3,44 @@
 
 import os
 from pathlib import Path
-from zipfile import ZIP_STORED, ZipFile
+from zipfile import ZIP_DEFLATED, ZIP_STORED, ZipFile
 
 from termcolor import cprint
 
 ZIP_MTIME_MIN = 315644400.0  # 1 day after 1980 for timezones
 
 
-def _get_src_file_mtime(src_file_path):
+def _get_src_file_mtime(src_file_path: Path):
     """Get source file mtime, but alter it if pkzip will reject it."""
     src_file_mtime = src_file_path.stat().st_mtime
     if src_file_mtime < ZIP_MTIME_MIN:
-        cprint(f"Updating mtime for zip compatibility: {src_file_path}", "yellow")
+        cprint(f"\tUpdating mtime for zip compatibility: {src_file_path}", "yellow")
         src_file_path.touch()
         src_file_mtime = src_file_path.stat().st_mtime
     return src_file_mtime
 
 
-def _check_for_updated_files(epub_path, src_dir, args):
-    """Check for updated files."""
-    archive_mtime = epub_path.stat().st_mtime if Path(epub_path).exists() else 0.0
+def get_dest_file_mtime(filename: Path, args):
+    """Get Destination file mtime."""
+    dest_path = args.dest / filename.name
+    return dest_path.stat().st_mtime if dest_path.exists() else 0.0
 
+
+def _get_updated_files(archive_mtime: float, src_dir: Path, args):
+    """Check for updated files."""
     src_paths = set()
     update = False
 
     for root, _, src_files in os.walk(src_dir):
-        rp = Path(root)
-        for src_filename in src_files:
-            src_file_path = rp / src_filename
+        root_path = Path(root)
+        for src_filename in sorted(src_files):
+            src_file_path = root_path / src_filename
             src_paths.add(src_file_path)
             src_file_mtime = _get_src_file_mtime(src_file_path)
             update = update or src_file_mtime > archive_mtime
 
     if not update and not args.force:
-        src_paths = False
+        src_paths = set()
 
     return src_paths
 
@@ -46,28 +50,24 @@ def _archive_epub(epub_path, src_paths, args):
     cprint(f"\nArchiving: {epub_path.name}", "cyan")
     new_epub_path = epub_path.with_suffix(".epub_new")
 
-    with ZipFile(new_epub_path, "w") as epub:
+    with ZipFile(
+        new_epub_path, mode="w", compression=ZIP_DEFLATED, compresslevel=9
+    ) as epub:
         for src_file_path in src_paths:
-            ctype = ZIP_STORED if src_file_path.name == "mimetype" else None
+            ctype = ZIP_STORED if src_file_path.name == "mimetype" else ZIP_DEFLATED
             if args.verbose:
                 cprint(f"\t{src_file_path}", "cyan")
-            epub.write(src_file_path, compress_type=ctype, compresslevel=9)
+            epub.write(src_file_path, compress_type=ctype)
 
     # Move tempfile over old epub
     new_epub_path.rename(epub_path)
 
 
-def backup_epub_dir(filename, args):
+def backup_epub_dir(epub_dir_path, args):
     """Compress the exploded epub dir to the backup destination."""
-    epub_path = args.dest / filename.name
-
-    src_paths = _check_for_updated_files(epub_path, filename, args)
-
-    if not src_paths:
-        if args.verbose:
-            cprint(f"Epub contents not updated, skipping: {epub_path}", "green")
-        else:
-            cprint(".", "green", end="")
-        return
-
-    _archive_epub(epub_path, src_paths, args)
+    dest_path = args.dest / epub_dir_path.name
+    archive_mtime = get_dest_file_mtime(epub_dir_path, args)
+    if src_paths := _get_updated_files(archive_mtime, epub_dir_path, args):
+        _archive_epub(dest_path, src_paths, args)
+        return True
+    return False
