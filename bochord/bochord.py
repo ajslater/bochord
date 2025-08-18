7c59233b21f3ff1f6152595da9b041f40ca78428
AJ Slater <aj@slater.net>
2025-08-18 13:38:23 -0700

    retry if source dir not ready

--- bochord/bochord.py
+++ bochord/bochord.py
@@ -3,10 +3,14 @@
 import shutil
 from argparse import Namespace
 from pathlib import Path
+from time import sleep
 
 from termcolor import cprint
 
 from bochord.epub_dir import backup_epub_dir, get_dest_file_mtime
+
+RETRIES = 3
+RETRY_SLEEP = 3
 
 
 def backup_file(filename: Path, args: Namespace) -> bool:
@@ -34,9 +38,29 @@ def prune(args: Namespace) -> None:
         cprint("\tRemoved: {filename}", "yellow")
 
 
+def read_source_dir(source: Path):
+    """List source dir that may not be ready yet."""
+    try_num = 0
+    for try_num in range(RETRIES):
+        try:
+            filenames = sorted(source.iterdir())
+        except InterruptedError as exc:
+            cprint(f"Retrying {try_num}: {exc}")
+            sleep(RETRY_SLEEP)
+            continue
+        except:
+            raise
+        break
+    else:
+        reason = f"Tried {try_num} times and failed to list files in {source}"
+        raise ValueError(reason)
+    return filenames
+
+
 def run(args: Namespace) -> None:
     """Backup everything."""
-    for filename in sorted(args.source.iterdir()):
+    filenames = read_source_dir(args.source)
+    for filename in filenames:
         backup_func = (
             backup_epub_dir
             if filename.suffix == ".epub" and filename.is_dir()
